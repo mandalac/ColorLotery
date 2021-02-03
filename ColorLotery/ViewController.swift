9c625b1c48e713376c8b39b9209d3919546f9a09
Vasili Mandalac <mandalacvasili1991@gmail.com>
2021-02-03 10:27:56 -0600

    Initial Commit

---
+++ ColorLotery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorLotery
+//
+//  Created by Vasili Mandalac on 2/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
